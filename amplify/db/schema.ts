0db1196a72e90b7cc7bf7a4240007f66eeda97dc
jf22201 <jf22201@proton.me>
2026-02-19 10:32:48 +1100

    Modified drizzle schema + config to support multi-dev database

--- amplify/db/schema.ts
+++ amplify/db/schema.ts
@@ -1,31 +1,36 @@
-import { integer, pgTable, varchar, text } from "drizzle-orm/pg-core";
+import { integer, pgTable, varchar, text, pgSchema } from "drizzle-orm/pg-core";
 
-export const usersTable = pgTable("users", {
+export const schemaName = process.env.DB_SCHEMA || "public"; //Use the current dev's schema name if defined otherwise fall back to public (prod should only be only a public schema)
+export const currentSchema = pgSchema(schemaName);
+export const usersTable = currentSchema.table("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   userId: varchar({ length: 255 }).notNull(),
   email: varchar({ length: 255 }).notNull(),
 });
 
-export const vendorsTable = pgTable("vendors", {
+export const vendorsTable = currentSchema.table("vendors", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   vendor_name: varchar({ length: 255 }),
 });
 
-export const comparisonGroupsTable = pgTable("comparison_groups", {
+export const comparisonGroupsTable = currentSchema.table("comparison_groups", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   user_id: integer().references(() => usersTable.id, { onDelete: "cascade" }),
   price_alert: integer().default(0), //using integer as we are storing price in cents.
 });
 
-export const comparisonProductsTable = pgTable("comparison_products", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  product_id: integer().references(() => productsTable.id),
-  group: integer().references(() => comparisonGroupsTable.id, {
-    onDelete: "cascade",
-  }),
-});
+export const comparisonProductsTable = currentSchema.table(
+  "comparison_products",
+  {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    product_id: integer().references(() => productsTable.id),
+    group: integer().references(() => comparisonGroupsTable.id, {
+      onDelete: "cascade",
+    }),
+  },
+);
 
-export const productsTable = pgTable("products", {
+export const productsTable = currentSchema.table("products", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   product_name: text().notNull(),
   price: integer().notNull(),
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,9 +3,10 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   out: "./drizzle",
-  schema: "./src/db/schema.ts",
+  schema: "./amplify/db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
   },
+  schemaFilter: [process.env.DB_SCHEMA || "public"],
 });
