bc8c77758c3f6d59dec4de270c84f1cd5f738b40
jf22201 <jf22201@proton.me>
2026-02-19 17:46:46 +1100

    WIP - handler logic

--- amplify/auth/post-confirmation/handler.ts
+++ amplify/auth/post-confirmation/handler.ts
@@ -1,5 +1,5 @@
 import type { PostConfirmationTriggerHandler } from "aws-lambda";
-
+import { db } from "@/amplify/index";
 export const handler: PostConfirmationTriggerHandler = (event) => {
   //extract relevant user info from trigger event
   const userid = event.request.userAttributes?.sub;
