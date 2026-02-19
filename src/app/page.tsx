0dcdcc625a1686fb69c429e4d894c6a6e6b5a08e
Rohan Redkar <rohanredkar@Rohans-MacBook-Air.local>
2026-02-19 17:05:48 +1100

    Added the 'add product' form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,23 @@
 "use client";
+import AddProduct from "@/components/AddProduct";
 import useAuth from "../../hooks/useAuth";
 import { signOut } from "aws-amplify/auth";
+import { useState } from "react";
 export default function Page() {
   const { user, loading } = useAuth();
+  // const [ showModal, setShowModal ] = useState(false);
   const logout = async () => {
     await signOut();
   };
+  const [showModal,setShowModal] = useState(false);
   return (
     <div>
       <h1>home</h1>
       <button onClick={logout}>Sign Out</button>
+      <button onClick={() => {
+        setShowModal(true);
+      }}>asldjandsa</button>
+      <AddProduct openTrigger={showModal} setOpenTrigger={setShowModal}/>
     </div>
-  );
-}
+    
+  );}
---
+++ src/components/AddProduct.tsx
@@ -0,0 +1,92 @@
+"use client";
+import { ButtonHTMLAttributes, SetStateAction, useState, Dispatch } from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  CardContent,
+} from "@/components/ui/card";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+
+ export default function AddProduct({openTrigger, setOpenTrigger}:{openTrigger:boolean,setOpenTrigger:Dispatch<SetStateAction<boolean>>}) {
+  const [url1, setURL1] = useState("");
+  const [url2, setURL2] = useState("");
+  const [priceAlert, setpriceAlert] = useState("");
+  const [productName, setproductName] = useState("");
+  async function addProduct() {
+  }
+  return (
+    <Dialog open={openTrigger} onOpenChange={setOpenTrigger}>
+        <DialogTrigger></DialogTrigger>
+        <DialogContent
+        onPointerDownOutside={(e) => e.preventDefault()}
+        >
+        <DialogHeader>
+            <DialogTitle>Add Product</DialogTitle>
+        </DialogHeader>
+            <CardContent className="space-y-4">
+                <form onSubmit={addProduct} action="" className="space-y-4">
+                <div className="space-y-2">
+                    <Label htmlFor="name">Product Name</Label>
+                    <Input
+                    id="text"
+                    type="text"
+                    value={productName}
+                    placeholder="Name"
+                    onChange={(e) => setproductName(e.target.value)}
+                    required
+                    />
+                </div>
+                <div className="space-y-2">
+                    <Label htmlFor="url1">Coles URL</Label>
+                    <Input
+                    id="text"
+                    type="text"
+                    value={url1}
+                    placeholder="www.coles.com.au/..."
+                    onChange={(e) => setURL1(e.target.value)}
+                    required
+                    />
+                </div>
+                <div className="space-y-2">
+                    <Label htmlFor="url2">Woolworths URL</Label>
+                    <Input
+                    id="text"
+                    type="text"
+                    placeholder="www.woolworths.com.au/..."
+                    value={url2}
+                    onChange={(e) => setURL2(e.target.value)}
+                    required
+                    />
+                </div>
+                <div className="space-y-2">
+                    <Label htmlFor="priceAlert">Price Alert</Label>
+                    <Input
+                    id="text"
+                    type="text"
+                    value={priceAlert}
+                    placeholder="$$$"
+                    onChange={(e) => setpriceAlert(e.target.value)} 
+                    />
+                </div>
+                {
+                    <Button
+                    type="submit"
+                    className="w-full hover:bg-primary/50"
+                    variant="outline"
+                    >
+                    Add
+                    </Button>
+                }
+                </form>
+            </CardContent>
+        </DialogContent>
+    </Dialog>
+
+  );}
