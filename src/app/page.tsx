a17bcb2d1aad11abdff579b88777b25a4fd4dfbf
brownstickman <rohanredkar01@gmail.com>
2026-02-24 15:53:46 +1100

    WIP: feat: working on homepage product listing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,51 @@
 "use client";
+
 import AddProduct from "@/components/AddProduct";
 import useAuth from "../../hooks/useAuth";
 import { signOut } from "aws-amplify/auth";
 import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+import { useRouter } from "next/navigation";
 export default function Page() {
+  const router = useRouter();
   const { user, loading } = useAuth();
-  // const [ showModal, setShowModal ] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+
   const logout = async () => {
     await signOut();
+    router.push("/login")
   };
-  const [showModal,setShowModal] = useState(false);
+
   return (
-    <div>
-      <h1>home</h1>
-      <button onClick={logout}>Sign Out</button>
-      <button onClick={() => {
-        setShowModal(true);
-      }}>asldjandsa</button>
-      <AddProduct openTrigger={showModal} setOpenTrigger={setShowModal}/>
+    <div className="min-h-screen flex flex-col">
+      
+      <div className="w-full bg-black px-6 py-3 flex justify-between items-center gap-3">
+        
+
+        <Button
+          variant="secondary"
+          className="bg-white text-black hover:bg-gray-200"
+          size="icon"
+          onClick={() => setShowModal(true)}
+        >
+          <Plus className="h-5 w-5" />
+        </Button>
+
+        <Button
+          variant="secondary"
+          className="bg-white text-black hover:bg-gray-200"
+          onClick={logout}
+        >
+          Sign Out
+        </Button>
+      </div>
+
+      <div className="p-6">
+        <h1 className="text-2xl font-semibold">Home</h1>
+      </div>
+
+      <AddProduct openTrigger={showModal} setOpenTrigger={setShowModal} />
     </div>
-    
-  );}
+  );
+}
