c8ce683b3266fa7974a3d05066d540ff319fb51a
jf22201 <jf22201@proton.me>
2026-03-05 18:21:15 +1100

    chore: add documentation + error handling for getParser function

--- collector/parsers/index.ts
+++ collector/parsers/index.ts
@@ -1,6 +1,11 @@
 import { parseColesPage } from "./coles";
 import { parseWoolworthsPage } from "./woolworths";
 
+/**
+ * Returns the appropriate price parser function for a given vendor
+ * @param vendor_slug - vendor identifier i.e 'coles','woolworths'
+ * @returns a parser function that will extract product name and price from html.
+ */
 export function getParser(
   vendor_slug: string,
 ): (html: string) => { name: string; price: number } {
@@ -12,5 +17,9 @@ export function getParser(
     coles: parseColesPage,
     //NOTE: whenever a new vendor parser is created, need to insert its record here.
   };
-  return parsers[vendor_slug];
+  const parser = parsers[vendor_slug];
+  if (!parser) {
+    throw new Error(`No parser found for ${vendor_slug}`);
+  }
+  return parser;
 }
